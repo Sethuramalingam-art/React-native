2253cf513826d31d2fc7f044013782e715c48efd
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2023-12-05 23:03:12 +0530

    Update index.js

--- employee-jobs/my-app/app/index.js
+++ employee-jobs/my-app/app/index.js
@@ -1,9 +1,18 @@
-import { View, Text } from "react-native";
-
+import { useState } from "react";
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
 export default function Home() {
+  const router = useRouter();
   return (
-    <View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Text>Home</Text>
-    </View>
+    </SafeAreaView>
   );
 }
